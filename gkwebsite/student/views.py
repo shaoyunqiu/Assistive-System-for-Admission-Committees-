e061ffe42f5842e3d39d7d536bb9fbab54d2dac5
段清楠 <dqn14@mails.tsinghua.edu.cn>
2016-10-01 22:01:17 +0800

    Halfway

--- gkwebsite/student/views.py
+++ gkwebsite/student/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+import datetime
 
 # Create your views here.
 
-def hello(request):
-	return HttpResponse("Hello, world!")
+def student_center(request):
+	t = get_template('student/center.html')
+	c = Context({'person_name': 'John Smith',
+		'company': 'Outdoor Equipment',
+		'ship_date': datetime.date(2009, 4, 2),
+		'ordered_warranty': False})
+	return HttpResponse(t.render(c))
