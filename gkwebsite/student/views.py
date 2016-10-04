5a70a6178402d9e830a4c9dbd2dde95a3838a340
段清楠 <dqn14@mails.tsinghua.edu.cn>
2016-10-05 00:42:20 +0800

    Introduce django session Completed full skeleton(center, score, rank, admit, contact) Next: introduce AJAX and confirm interfaces of frontend/backend communication

--- gkwebsite/student/views.py
+++ gkwebsite/student/views.py
@@ -9,5 +9,23 @@ import datetime
 
 def student_center(request):
 	t = get_template('student/center.html')
-	c = Context({'name': 'duanqn'})
+	c = Context({'name': 'duanqn', 'id': 2014011425})
+	#request.session['user_id'] = 2014011425
 	return HttpResponse(t.render(c))
+	
+def student_score(request):
+	t = get_template('student/score.html')
+	id = request.session.get('user_id', -1)
+	if id == -1:
+		return HttpResponse('Access denied')
+	c = Context({'id': id})
+	return HttpResponse(t.render(c))
+	
+def student_rank(request):
+	return HttpResponse('This is RANK page.')
+	
+def student_admit(request):
+	return HttpResponse('This is ADMIT page.')
+	
+def student_contact(request):
+	return HttpResponse('This is CONTACT page.')
