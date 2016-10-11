22734bcdc4a399c36d5b53496ce85cf464784dc8
段清楠 <dqn14@mails.tsinghua.edu.cn>
2016-10-11 16:35:00 +0800

    Completed nav-bar on left side and top right corner.

--- gkwebsite/teacher/urls.py
+++ gkwebsite/teacher/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import *
 import teacher_py.userinfo as userinfo
+from . import views
 
 urlpatterns = [
     url(r'^index/', userinfo.index),
+		url(r'^search_student/$', views.search_student, name='search_student_teacher'),
     ]
--- gkwebsite/teacher/views.py
+++ gkwebsite/teacher/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
+from django.template.loader import get_template
+from django.template import Context
+
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def search_student(request):
+	t = get_template('teacher/list_student.html')
+	c = Context({})
+	return HttpResponse(t.render(c))
