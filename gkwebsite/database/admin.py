4bda035051d267bb9a1d3a80617826a48d50ec00
lihy96 <lhy237451594@vip.qq.com>
2016-10-11 18:14:26 +0800

    add Student Table and Student Backend Interface

--- gkwebsite/database/admin.py
+++ gkwebsite/database/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 import models
 admin.site.register(models.Teacher)
-#admin.site.register(models.Student)
+admin.site.register(models.Student)
