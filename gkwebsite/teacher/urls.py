d18b9bd5f6f1bf8a64c8fea2439de11dfaaa0c6f
bbaiyhaor <360559261@qq.com>
2016-10-12 20:15:53 +0800

    Refactor the profile files to keep the project clean and readable.

--- gkwebsite/teacher/urls.py
+++ gkwebsite/teacher/urls.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 from django.conf.urls import *
-import teacher_py.userinfo as userinfo
 from . import views
 
-urlpatterns = [url(r'^$', userinfo.index),
+urlpatterns = [
                url(r'^search_student/$', views.search_student, name='search_student_teacher'),
 
                # teacher 查看修改个人信息
-               url(r'^profile/', userinfo.profile), ]
+               url(r'^profile/', views.profile),
+
+                ]
--- gkwebsite/teacher/views.py
+++ gkwebsite/teacher/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+#encoding=utf-8
+from django.http import JsonResponse
+from django.shortcuts import render_to_response
 from django.http import HttpResponse, request
+from django.template import RequestContext
 from django.template.loader import get_template
 from django.template import Context
-
-from django.shortcuts import redirect
-
 # Create your views here.
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
 
 def search_student(request):
-	t = get_template('teacher/list_student.html')
-	c = Context({})
-	return HttpResponse(t.render(c))
+    t = get_template('teacher/list_student.html')
+    c = Context({})
+    return HttpResponse(t.render(c))
+
+
+'''
+    查看和修改教师个人信息
+    by byr 161012
+'''
+#@csrf_protect
+@csrf_exempt
+def profile(request):
+    if request.method == 'POST':
+        print "heheda"
+        teacher_name = request.POST.get('teacher_name', 'byr')
+        phone = request.POST.get('phone', '110')
+        dict = {'teacher_name': teacher_name, 'email': '1', 'work_address': '2', 'home_address': '10', 'postcode': '3',
+                'homephone': '4', 'phone': phone, 'qqn': '5', 'weichat': '6', 'describe': '7', }
+        return JsonResponse(dict)
+    else:
+        return render_to_response('userinfo.html',context_instance=RequestContext(request))
