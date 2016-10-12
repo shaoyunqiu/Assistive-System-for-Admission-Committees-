8a2eb1471e1b1a0949167c28711946e1731424e3
bbaiyhaor <360559261@qq.com>
2016-10-12 19:20:09 +0800

    Add looking at and modifying teacher profile UI.

--- gkwebsite/teacher/urls.py
+++ gkwebsite/teacher/urls.py
@@ -1,8 +1,10 @@
+# coding=utf-8
 from django.conf.urls import *
 import teacher_py.userinfo as userinfo
 from . import views
 
-urlpatterns = [
-    url(r'^index/', userinfo.index),
-		url(r'^search_student/$', views.search_student, name='search_student_teacher'),
-    ]
+urlpatterns = [url(r'^$', userinfo.index),
+               url(r'^search_student/$', views.search_student, name='search_student_teacher'),
+
+               # teacher 查看修改个人信息
+               url(r'^profile/', userinfo.profile), ]
