758ab97cc1d964ef08946b9e73bc06b8db53a315
shaoyunqiu <shaoyunqiu@163.com>
2016-10-12 15:24:40 +0800

    finish the student_info.html and student_info_edit.html,one editable and the other one uneditable, matches the urls , can run server, but url matches have some problems

--- gkwebsite/teacher/urls.py
+++ gkwebsite/teacher/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^index/', userinfo.index),
 		url(r'^search_student/$', views.search_student, name='search_student_teacher'),
+    url(r'^student_info_edit/$', views.student_info_edit),
+    url(r'^student_info_edit/formsubmit/$', views.student_info_save),
+    url(r'^student_info/edit/$', views.student_info_edit),
+    url(r'^student_info/$', views.student_info_show),
     ]
--- gkwebsite/teacher/views.py
+++ gkwebsite/teacher/views.py
@@ -11,3 +11,19 @@ def search_student(request):
 	t = get_template('teacher/list_student.html')
 	c = Context({})
 	return HttpResponse(t.render(c))
+
+def student_info_edit(request):
+	t = get_template('teacher/student_info_edit.html')
+	c = Context({})
+	return  HttpResponse(t.render(c))
+
+
+def student_info_save(request):
+	t = get_template('techer/student_info.html')
+	c = Context({})
+	return HttpResponse(t.render(c))
+
+def student_info_show(request):
+	t = get_template('teacher/student_info.html')
+	c = Context({})
+	return HttpResponse(t.render(c))
