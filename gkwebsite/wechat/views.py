69ab0c15a592254d56dc992d1e88a70074662abb
shaoyunqiu <shaoyunqiu@163.com>
2016-10-16 14:11:11 +0800

    link to the wechat server success fully, timely use the token appid and appsecrete as the global var, finish the get method

--- gkwebsite/wechat/views.py
+++ gkwebsite/wechat/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+import hashlib
+
+Token = "zaoshuizaoqi"
+Appid = "wxd1c16a4667e24faf"
+Appsecrete = "efe75bfad99903dff1ba7a783a354e71"
+
+def wechat_main(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == "GET":
+        signature = request.GET.get('signature', None)
+        timestamp = request.GET.get('timestamp', None)
+        nonce = request.GET.get('nonce', None)
+        echostr = request.GET.get('echostr', None)
+        token = "zaoshuizaoqi"
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("weixin  index")
+    
