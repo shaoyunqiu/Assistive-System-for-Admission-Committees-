1bc1598f9a1e2591ee7aa8eb09b2caa92bcaca51
段清楠 <dqn14@mails.tsinghua.edu.cn>
2016-10-17 10:09:26 +0800

    Modify URL of teacher.

--- gkwebsite/teacher/urls.py
+++ gkwebsite/teacher/urls.py
@@ -3,17 +3,16 @@ from django.conf.urls import *
 from . import views
 
 urlpatterns = [
-		url(r'^student_info_edit/$', views.student_info_edit),
-		url(r'^student_info_edit/formsubmit/$', views.student_info_save),
-		url(r'^student_info/edit/$', views.student_info_edit),
-		url(r'^student_info/$', views.student_info_show),
-		url(r'^fake_backend/$', views.fake_backend),
-		url(r'^logout/$', views.teacher_logout),
+		url(r'^student_info_edit/$', views.student_info_edit, name='student_info_edit'),
+		url(r'^student_info_edit/formsubmit/$', views.student_info_save, name='student_info'),
+		url(r'^student_info/edit/$', views.student_info_edit, name='student_info'),
+		url(r'^student_info/$', views.student_info_show, name='student_info'),
+		url(r'^logout/$', views.teacher_logout, name='logout'),
 		url(r'^search_student/$', views.search_student, name='search_student'),
 		url(r'^search_volunteer/$', views.search_volunteer, name='search_volunteer'),
 		url(r'^add_student/$', views.add_student, name='add_student'),
 		# teacher 查看修改个人信息
 		url(r'^profile/$', views.profile),
-		url(r'^$', views.dashboard),
-
+		url(r'^$', views.dashboard, name='dashboard'),
+		url(r'^add_volunteer/$', views.add_volunteer, name='add_volunteer')
 ]
