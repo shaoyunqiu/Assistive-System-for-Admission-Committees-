855f634fe52bec7963b17e0a80541800936c9125
lihy96 <lhy237451594@vip.qq.com>
2016-10-21 15:04:48 +0800

    indent

--- gkwebsite/student/urls.py
+++ gkwebsite/student/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.student_center, name='student_center'),
-	url(r'^score/$', views.student_score, name='student_score'),
-	url(r'^contact/$', views.student_contact, name='student_contact'),
-	url(r'^rank/$', views.student_rank, name='student_rank'),
-	url(r'^admit/$', views.student_admit, name='student_admit'),
-	url(r'^logout/$', views.student_logout, name='student_logout'),
-	url(r'^profile/$', views.profile)
+    url(r'^$', views.student_center, name='student_center'),
+    url(r'^score/$', views.student_score, name='student_score'),
+    url(r'^contact/$', views.student_contact, name='student_contact'),
+    url(r'^rank/$', views.student_rank, name='student_rank'),
+    url(r'^admit/$', views.student_admit, name='student_admit'),
+    url(r'^logout/$', views.student_logout, name='student_logout'),
+    url(r'^profile/$', views.profile)
 ]
