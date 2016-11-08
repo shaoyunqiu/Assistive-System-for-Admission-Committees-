044b2ac354f9ede33bc15c095d4469747e050e50
ahaoyunqiu <shaoyunqiu@163.com>
2016-11-08 23:57:03 +0800

    write get the open id ,but not sure can run

--- gkwebsite/wechat/urls.py
+++ gkwebsite/wechat/urls.py
@@ -1,7 +1,8 @@
 
 from django.conf.urls import url
-from . import views
+from . import views, wechat_api
 
 urlpatterns = [
     url(r'^$', views.wechat_main, name='wechat_main'),
+    url(r'^auth/$', wechat_api.authority, name="authority")
 ]
---
+++ gkwebsite/wechat/wechat_api.py
@@ -0,0 +1,57 @@
+#coding=utf-8
+
+# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from xml.etree import ElementTree as ET
+from django.utils.encoding import smart_str, smart_unicode
+import hashlib
+import urllib
+import urllib2
+import json
+import time
+import sys
+import requests
+reload(sys)
+sys.setdefaultencoding('UTF-8')
+
+
+Token = "zaoshuizaoqi"
+Appid = "wxd1c16a4667e24faf"
+Appsecret = "efe75bfad99903dff1ba7a783a354e71"
+server_url = "https://gaokao.northeurope.cloudapp.azure.com/"
+basic_scope = "snsapi_base"
+
+# open the authority url
+def authority(request):
+    re_dir = server_url + "login/"
+    url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=%s&scope=%s&state=%s#wechat_redirect"%(Appid, re_dir, "code",basic_scope, "1")
+    result = urllib2.urlopen(url)
+
+
+# get code
+def get_code(request):
+    if request.method == 'GET':
+        code = request.GET.get('code', None)
+        if code == None:
+            return (False, code)
+        else:
+            return (True, code)
+    else:
+        return (False, None)
+
+
+def get_openid_byCode(_code):
+    url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=%s" % (Appid, Appsecret,_code, "authorization_code")
+    try:
+        result = urllib2.urlopen(url)
+    except:
+        print "open the url failed"
+        return (False, None)
+
+    data = json.load(result)
+    if data.has_key('openid'):
+        openid = data['openid']
+        return (True, openid)
+    else:
+        return (False, None)
